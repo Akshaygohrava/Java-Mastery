283e36d76a108e7367cf9e9b6c564b9a01ce2b47
AkshayGohrava <akshaygohrava@gmail.com>
2026-07-02 20:45:59 +0530

    initial chnages

---
+++ Java-DSA-Programms/FindNonRepeatCharOut.java
@@ -0,0 +1,10 @@
+public class FindNonRepeatCharOut {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java DSA !");
+        System.out.println("Find Non Repeating Char Out !");
+        
+        String text = "aabbccdde";
+
+        
+    }
+}
---
+++ Java-Projects/Java-Calculator/Calculator.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Hello, This is Calculator Based On Java ,By Akshay-Gohrava");
+
+        System.out.println("==============================");
+        System.out.println("Welcome to Java Calculator");
+        System.out.println("==============================");
+        
+        Scanner cal = new Scanner(System.in);
+
+
+        System.out.println("Enter Number : ");
+        int input1 = cal.nextInt();
+
+        System.out.println("Enter Number 2 : ");
+        int input2 = cal.nextInt();
+
+        System.out.println("Addition is : "+input1 + input2);
+        
+
+        
+        
+
+
+
+
+
+
+    }
+ }
+
+
+
+
+
+ 
